7f634b24721eadd4df96527188458616ec2bdd6b
tw <>
2022-05-14 20:09:42 -0700

    Deploy Next.js to gh-pages

---
+++ out/_next/static/rJPJKuXFFCdw3Zf6SBMXH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8a4b8f2c65cf1afa.js"],"/_error":["static/chunks/pages/_error-0cb2bff341a47607.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
