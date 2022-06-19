2f2705a0665ded2db5af30b8c5cebbfb31d6d388
tw <>
2022-06-18 19:05:50 -0700

    adds link to new issue on main page

--- src/pages/index.page.jsx
+++ src/pages/index.page.jsx
@@ -9,6 +9,18 @@ export default function Home() {
       <ol>
         <ul>
           <Link
+            href="/radix-dialog-mobile-safari-ios-15-jumps-on-scroll-when-address-bar-height-animates"
+            as={`${projectDir}/radix-dialog-mobile-safari-ios-15-jumps-on-scroll-when-address-bar-height-animates`}
+          >
+            <a>Issue: Radix Dialog in mobile Safari iOS 15 jumps on scroll when address bar height animates</a>
+          </Link>
+          {' '}
+          <em>Created 2022-06-18</em>
+        </ul>
+        <ul>
+          FIXED:
+          {' '}
+          <Link
             href="/radix-dialog-mobile-eats-first-pointer-event-after-dismiss"
             as={`${projectDir}/radix-dialog-mobile-eats-first-pointer-event-after-dismiss`}
           >
